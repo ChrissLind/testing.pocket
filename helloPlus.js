2c7020a9931ca268722ec7b52dcd4f4fcac772e0
ChrissLind <chrislindino@gmail.com>
2022-10-20 15:29:46 -0400

    adding 2 js files for testing

---
+++ helloPlus.js
@@ -0,0 +1,12 @@
+// Simple console.log, no semi colon
+console.log("Hello")
+
+// Simple console.log, with semi colon
+console.log("Hello");
+
+// console.log variable that changes
+let string = "Hello";
+console.log(string);
+string = "World!";
+console.log(string);
+
