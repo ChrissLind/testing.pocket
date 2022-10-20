24febffe7f559d9aa40c34dad161f6ff71cbba16
ChrissLind <chrislindino@gmail.com>
2022-10-20 16:21:20 -0400

    Coqui counter

--- helloPlus.js
+++ helloPlus.js
@@ -69,3 +69,12 @@ for (let i = 1; i <= number; i++) {
     n1 = n2;
     n2 = nextTerm;
 }
+
+// console.log up to 1000 coqui's
+function countCoqui(limit){
+    for (var i = 1; i < limit; i +=1){
+        console.log(i + " Coqui(s)")
+    }
+}
+
+countCoqui(10);
